6840f3f82ea3f9c2a3035f28713a0ac884c6d72d
Martin Swiderski <martin.swiderski@gmail.com>
2016-01-16 21:58:24 +0000

    wip

---
+++ temp2.js
@@ -0,0 +1,9 @@
+
+var short     = require('./src/short'),
+    urlString = 'http://business.worldpay.com/merchant-services?mh_keyword=%2Bcard%20%2Bpayments&campaignID=194165972&campaign={campaign}&medium={cpc}&source={google}&keyword=%2Bcard%20%2Bpayments&gclid=CjwKEAiA8ee0BRD1l7vV6JHe0zISJADxYItmkKtIFPlNPoYNOM_s4gILJe_5-jyplwONDbsAcuQTFhoC7pLw_wcB';
+
+
+
+console.log('ME');
+console.log(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(urlS););
+console.log(short.parsed(urlString));
