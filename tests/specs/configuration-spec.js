0f7d2d640c7601db99160687e5cee46ccdd31d97
Martin Swiderski <martin.swiderski@gmail.com>
2016-01-17 18:08:26 +0000

    config

--- tests/specs/configuration-spec.js
+++ tests/specs/configuration-spec.js
@@ -1,15 +1,17 @@
 process.env['SHORT_URL_MOCK']      = 'A';
 process.env['SHORT_URL_MIN']       = 'B';
 process.env['SHORT_URL_MAX']       = 'C';
-process.env['SHORT_URL_PARSE_SRC'] = 'D';
+process.env['SAME_CHAR_MAX']       = 'D';
+process.env['SHORT_URL_PARSE_SRC'] = 'E';
 
 var configuration = require(__dirname + "/../../src/configuration");
 
 describe("Reads from env vars", function () {
     it("regardless how they are sey", function () {
-        expect(configuration.hostname).toBe('A');
-        expect(configuration.protocol).toBe('B');
-        expect(configuration.path).toBe('C');
-        expect(configuration.token).toBe('D');
+        expect(configuration.get("mock")).toBe('A');
+        expect(configuration.get("min")).toBe('B');
+        expect(configuration.get("max")).toBe('C');
+        expect(configuration.get("sameCharMax")).toBe('D');
+        expect(configuration.get("parseUrl")).toBe('E');
     });
 });
